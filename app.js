641a92d297307b2235dac801b3009184cc56aa01
Sepehr Mohammadi <mohamadi.sepehr@live.com>
2017-05-12 18:19:15 +0430

    changed app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-/**
- * Created by Sepehr on 5/12/17.
- */
-console.log("Hello")
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+server.listen(server_port, server_ip_address, function () {
+    console.log("Listening on " + server_ip_address + ", port " + server_port)
+});
